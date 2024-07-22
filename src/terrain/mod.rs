84cc43e388a9430ebd2fd96f5bda53f83be8b4f2
Rulrn <rulrnovistky@gmail.com>
2024-07-22 23:04:39 +0200

    update terrain to add LOP

--- src/terrain/mod.rs
+++ src/terrain/mod.rs
@@ -5,9 +5,12 @@ use bevy::render::{
 };
 
 use noise::{NoiseFn, Perlin, Turbulence};
-const SIZE: Vec2 = Vec2::new(500.0, 500.0);
+
+// Terrain Constants
+const MAP_CHUNK_SIZE: usize = 240;
+const SIZE: Vec2 = Vec2::new(MAP_CHUNK_SIZE as f32, MAP_CHUNK_SIZE as f32);
 const AMPLITUDE: f64 = 10.0;
-const RES: f32 = 1.0;
+const LOD: usize = 0;
 const SCALE: f32 = 0.005;
 
 pub struct TerrainPlugin;
@@ -23,7 +26,7 @@ impl Plugin for TerrainPlugin {
 pub struct TerrainParams {
     size: Vec2,
     amplitude: f64,
-    res: f32,
+    lod: usize,
     frequency: f64,
     power: f64,
     roughness: usize,
@@ -38,7 +41,7 @@ fn build_terrain(
     q_terrain_params: Query<(Entity, &TerrainParams), Changed<TerrainParams>>,
 ) {
     for (entity, tp) in q_terrain_params.iter() {
-        let perlin = Perlin::new(rand::random());
+        let perlin = Perlin::new(548);
 
         let noise: Turbulence<Perlin, Perlin> = Turbulence::new(perlin)
             .set_frequency(tp.frequency)
@@ -65,7 +68,7 @@ fn create_terrain(mut cmds: Commands) {
     cmds.spawn(TerrainParams {
         size: SIZE,
         amplitude: AMPLITUDE,
-        res: RES,
+        lod: LOD,
         frequency: 0.2,
         power: 10.0,
         roughness: 4,
@@ -75,55 +78,62 @@ fn create_terrain(mut cmds: Commands) {
 
 fn create_cube_mesh(tp: &TerrainParams, noise: impl NoiseFn<f64, 2>) -> Mesh {
     // Keep the mesh data accessible in future frames to be able to mutate it in toggle_texture.
+    let (vertex_grid, vertex_indices) =
+        create_vertex_grid(tp.size, tp.amplitude, tp.lod, noise, tp.scale);
     Mesh::new(
         PrimitiveTopology::TriangleList,
         RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
     )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        create_vertex_grid(tp.size, tp.amplitude, tp.res, noise, tp.scale),
-    )
-    .with_inserted_indices(create_indices_grid(tp.size, tp.res))
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertex_grid)
+    .with_inserted_indices(vertex_indices)
     .with_computed_normals()
 }
 
 fn create_vertex_grid(
     size: Vec2,
     amplitude: f64,
-    res: f32,
+    lod: usize,
     noise: impl NoiseFn<f64, 2>,
     scale: f32,
-) -> Vec<Vec3> {
-    let xlen = (size.x * (1.0 / res)) as u32;
-    let ylen = (size.y * (1.0 / res)) as u32;
+) -> (Vec<Vec3>, Indices) {
+    let xlen = size.x as u32;
+    let ylen = size.y as u32;
+
+    let mesh_simplification_increment = match lod {
+        0 => 1,
+        _ => lod,
+    };
+    // let mesh_simplification_increment = 1;
+
+    let vertices_per_line = ((size.x - 1.0) / mesh_simplification_increment as f32 + 1.0) as u32;
+    // let vertices_per_line = size.x as u32;
 
     let mut grid = vec![];
-    for iy in 0..=ylen as usize {
-        for ix in 0..=xlen as usize {
-            let x = (ix as f32 * res) - (size.x / 2.0);
-            let y = (iy as f32 * res) - (size.y / 2.0);
+    let mut indices = vec![];
+    let mut vidx = 0;
+
+    dbg!(vertices_per_line);
+    for iy in (0..=ylen).step_by(mesh_simplification_increment) {
+        for ix in (0..=xlen).step_by(mesh_simplification_increment) {
+            // create vertices
+            let x = (ix as f32) - (size.x / 2.0);
+            let y = (iy as f32) - (size.y / 2.0);
             let z = noise.get([(x * scale) as f64, (y * scale) as f64]) as f32;
             grid.push(Vec3::new(x, z * amplitude as f32, y));
+
+            // create indices
+            if ix < xlen && iy < ylen {
+                dbg!(vidx);
+                indices.push(vidx);
+                indices.push(vidx + vertices_per_line + 1);
+                indices.push(vidx + 1);
+                indices.push(vidx + vertices_per_line + 1);
+                indices.push(vidx + vertices_per_line + 2);
+                indices.push(vidx + 1);
+            }
+            vidx += 1;
         }
     }
-    return grid;
-}
 
-fn create_indices_grid(size: Vec2, res: f32) -> Indices {
-    let xlen = (size.x * (1.0 / res)) as u32;
-    let ylen = (size.y * (1.0 / res)) as u32;
-
-    let mut indices = vec![];
-    for x in 0..xlen {
-        for y in 0..ylen {
-            let idx = y * (xlen + 1) + x;
-            indices.push(idx);
-            indices.push(idx + xlen + 1);
-            indices.push(idx + 1);
-            indices.push(idx + xlen + 1);
-            indices.push(idx + xlen + 2);
-            indices.push(idx + 1);
-        }
-    }
-    Indices::U32(indices)
+    (grid, Indices::U32(indices))
 }
