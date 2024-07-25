0f286cf40fe4b99b9e014556255046e48922aa7e
aviac <aviac@mailbox.org>
2024-07-25 21:32:56 +0200

    add docs to audio stuff

--- src/audio/ambient.rs
+++ src/audio/ambient.rs
@@ -12,7 +12,10 @@ impl Plugin for AmbientAudioPlugin {
         app.add_systems(
             Update,
             (
-                start_ambient_music.run_if(not(any_with_component::<AmbientMusic>)),
+                start_random_ambient_track.run_if(
+                    not(any_with_component::<AmbientMusic>)
+                        .and_then(resource_exists::<AmbientMusicHandles>),
+                ),
                 remove_finished_ambient_audio.run_if(
                     any_with_component::<AmbientMusic>.and_then(on_timer(Duration::from_secs(1))),
                 ),
@@ -21,22 +24,28 @@ impl Plugin for AmbientAudioPlugin {
     }
 }
 
+/// component attached to entity which plays the current background music. Normally there should
+/// only ever be one such entity
 #[derive(Debug, Clone, Component, Default, Reflect)]
 pub struct AmbientMusic;
 
+/// Title of an ambient music track
 #[derive(Debug, Clone)]
-pub struct Title(String);
-impl std::fmt::Display for Title {
+pub struct TrackTitle(String);
+impl std::fmt::Display for TrackTitle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
 }
 
+/// All background ambient music handles which could possibly ever play
 #[derive(Debug, Clone, Resource, Deref, DerefMut)]
-pub struct AmbientMusicHandles(Vec<(Title, Handle<bevy_kira_audio::AudioSource>)>);
+pub struct AmbientMusicHandles(Vec<(TrackTitle, Handle<bevy_kira_audio::AudioSource>)>);
 
 impl FromWorld for AmbientMusicHandles {
     fn from_world(world: &mut World) -> Self {
+        // TODO: load this in a more async and non-blocking way
+        // load a fixed set of tracks on app start
         let asset_server = world.resource::<AssetServer>();
         let handles = [
             "bj5_desert",
@@ -47,7 +56,7 @@ impl FromWorld for AmbientMusicHandles {
         .map(|name| format!("{name}.mp3"))
         .map(|name| {
             (
-                Title(name.clone()),
+                TrackTitle(name.clone()),
                 asset_server.load(format!("audio/music/{name}")),
             )
         })
@@ -56,7 +65,7 @@ impl FromWorld for AmbientMusicHandles {
     }
 }
 
-fn start_ambient_music(
+fn start_random_ambient_track(
     mut cmds: Commands,
     ambient_music: Res<AmbientMusicHandles>,
     audio: Res<Audio>,
@@ -66,22 +75,12 @@ fn start_ambient_music(
         .expect("non empty");
     let audio_handle = audio
         .play(track.clone())
-        // The first 0.5 seconds will not be looped and are the "intro"
-        // .loop_from(0.5)
-        // Fade-in with a dynamic easing
         .fade_in(AudioTween::new(
             Duration::from_secs(2),
             AudioEasing::OutPowi(2),
         ))
-        // Only play on our right ear
-        // .with_panning(1.0)
-        // Increase playback rate by 50% (this also increases the pitch)
-        // .with_playback_rate(1.5)
-        // Play at half volume
         .with_volume(0.5)
         .handle();
-    // play the track reversed
-    // .reverse();
     cmds.spawn((
         Name::new(format!("Background: {track_name}")),
         AmbientMusic,
@@ -89,6 +88,8 @@ fn start_ambient_music(
     ));
 }
 
+// if a track is finished playing, despawn the related entity. This is important because the next
+// track will only start once there are no track entities in the world left
 fn remove_finished_ambient_audio(
     mut cmds: Commands,
     q_ambient_handles: Query<(Entity, &Handle<AudioInstance>), With<AmbientMusic>>,
--- src/audio/sfx.rs
+++ src/audio/sfx.rs
@@ -3,21 +3,29 @@ use bevy_kira_audio::prelude::*;
 
 use crate::camera::MainCamera;
 
+// controls the max distance at which you can barely hear spatial audio
+const SPATIAL_AUDIO_DISTANCE: f32 = 50.0;
+
 pub struct AudioSfxPlugin;
 impl Plugin for AudioSfxPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(SpatialAudio { max_distance: 50.0 })
-            .add_systems(Startup, init_sandstorm_emitter)
-            .add_systems(
-                Update,
-                init_camera_audio_receiver
-                    .run_if(any_with_component::<MainCamera>.and_then(run_once())),
-            );
+        app.insert_resource(SpatialAudio {
+            max_distance: SPATIAL_AUDIO_DISTANCE,
+        })
+        .add_systems(Startup, init_sandstorm_emitter)
+        .add_systems(
+            Update,
+            init_camera_audio_receiver
+                .run_if(any_with_component::<MainCamera>.and_then(run_once())),
+        );
     }
 }
 
 fn init_sandstorm_emitter(mut cmds: Commands, audio: Res<Audio>, asset_server: Res<AssetServer>) {
+    // load general sandstorm audio effect
     let sandstorm_handle = asset_server.load("audio/sfx/sandstorm-looping.mp3");
+
+    // closure which spawns spatial audio sandstorms at specific locations
     let mut i = 1;
     let spawn_sandstorm_at = |pos: Vec3| {
         cmds.spawn((
@@ -32,12 +40,15 @@ fn init_sandstorm_emitter(mut cmds: Commands, audio: Res<Audio>, asset_server: R
         ));
         i += 1;
     };
+
+    // spawn sandstorms everywhere but in the temple
     [Vec3::X, Vec3::NEG_X, Vec3::NEG_Z]
-        .map(|x| x * 40.0)
+        .map(|x| x * SPATIAL_AUDIO_DISTANCE * 0.8)
         .into_iter()
         .for_each(spawn_sandstorm_at);
 }
 
+// attach the spatial audio receiver component to the `MainCamera` entity for now
 fn init_camera_audio_receiver(mut cmds: Commands, camera: Query<Entity, With<MainCamera>>) {
     cmds.entity(camera.single()).insert(AudioReceiver);
 }
