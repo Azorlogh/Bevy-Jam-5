752dc212b83d4b83efe8507a53e42a53fa83dfc5
Azorlogh <bott.alix@gmail.com>
2024-07-27 02:30:44 +0200

    work

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,8 @@ fn setup(
         Name::new("Clocktower"),
         SceneBundle {
             scene: asset_server.load("levels/Hub.glb#Scene0"),
+            transform: Transform::from_xyz(100.0, -1.0, 0.0)
+                .with_rotation(Quat::from_rotation_x(0.1) * Quat::from_rotation_y(0.2)),
             ..default()
         },
     ));
