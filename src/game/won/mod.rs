1cd6a6c1d3b7359c86a7ecee467c6a9b43634010
aviac <aviac@mailbox.org>
2024-07-29 12:46:55 +0200

    improve win screen

--- src/game/won/mod.rs
+++ src/game/won/mod.rs
@@ -1,4 +1,5 @@
-use bevy::prelude::*;
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+use bevy_kira_audio::prelude::*;
 
 use crate::{
     menu::styling::{central_panel, default_text, opaque_root, PADDING},
@@ -10,7 +11,8 @@ use super::GameState;
 pub struct WonPlugin;
 impl Plugin for WonPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Won), setup_lose)
+        app.add_systems(OnEnter(GameState::Won), setup_win)
+            .add_systems(Update, setup_win.run_if(input_just_pressed(KeyCode::KeyX)))
             .add_systems(Update, interact_restart);
     }
 }
@@ -21,14 +23,15 @@ pub struct WonMenu;
 #[derive(Component)]
 pub struct Restart;
 
-fn setup_lose(mut cmds: Commands, asset_server: Res<AssetServer>) {
+fn setup_win(mut cmds: Commands, asset_server: Res<AssetServer>, audio: Res<Audio>) {
     cmds.insert_resource(SandstormIntensity(0.0));
+    audio.play(asset_server.load("audio/sfx/cheer.mp3"));
     cmds.spawn((opaque_root(), WonMenu, StateScoped(GameState::Won)))
         .with_children(|cmds| {
             cmds.spawn(central_panel()).with_children(|cmds| {
                 cmds.spawn(
                     default_text(
-                        "Good job!\nYou have fixed the weather machine!\nThe desert is peaceful once again.",
+                        "Good job!\nYou have fixed the weather machine!\nThe desert is peaceful once again.\n\n\nThanks for playing! <3",
                         64.0,
                         &asset_server,
                     )
